97b877f7269e045b2ffec744b8d0bdf9e7eebce2
Thomas Prasad <prasadt@tcd.ie>
2014-12-01 00:44:38 +0000

    fixed

--- app/build/generated/source/r/debug/com/example/android/opengl/R.java
+++ app/build/generated/source/r/debug/com/example/android/opengl/R.java
@@ -30,6 +30,7 @@ public final class R {
     public static final class raw {
         public static final int bayo_after_burner=0x7f040000;
         public static final int sound_file_1=0x7f040001;
+        public static final int untitled=0x7f040002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
